d1464349a9b4a1385ca8340e68d7946a55449ebc
Falasefemi2 <femifalase228@gmail.com>
2025-04-21 01:50:41 +0100

    Implement event retrieval endpoints and update Jupyter notebooks for API testing

--- src/api/events/routing.py
+++ src/api/events/routing.py
@@ -1,9 +1,18 @@
 from fastapi import APIRouter
+from .schema import EventSchema, EventListSchema
 
 router = APIRouter()
 
 @router.get("/")
-def read_events():
+def read_events() -> EventListSchema:
     return {
-        "items": [1,2,3]
+        "results": [
+            {"id": 1},
+            {"id": 2},
+            {"id": 3}
+        ]
     }
+    
+@router.get("/{event_id}")
+def get_event(event_id: int) -> EventSchema:
+    return {"id": event_id }
